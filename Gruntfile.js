e7c518dc654cdd1c1c98beb5a0690e6cc3905c53
Ben Creasy <contact@bencreasy.com>
2015-04-03 20:04:44 -0700

    (feat) Add initial reactjs components for hunt creation

--- Gruntfile.js
+++ Gruntfile.js
@@ -22,8 +22,8 @@ module.exports = function(grunt) {
 
     sass: {
       dev: {
-        src: ['Client/app/style/style.scss'],
-        dest: 'Client/app/style/style.css',
+        src: ['Client/style/style.scss'],
+        dest: 'Client/style/style.css',
       },
     },
 
@@ -54,7 +54,7 @@ module.exports = function(grunt) {
       },
       css: {
         files: [
-          'Client/app/style/*.css'
+          'Client/style/*.css'
         ],
         options: {
           livereload: true
@@ -71,7 +71,7 @@ module.exports = function(grunt) {
       },
       sass: {
         // We watch and compile sass files as normal but don't live reload here
-        files: ['Client/app/style/*.scss'],
+        files: ['Client/style/*.scss'],
         tasks: ['sass'],
       },
     }
